754a8b48d0935bc32d633b76426243063fa214c3
YuraDukhnoAman <yura.dukhno@aman.co.il>
2025-07-12 18:39:36 +0300

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,11 +2,15 @@ const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/disire-music-club-website" : "",
   compiler: {
     styledComponents: true,
   },
   images: {
     domains: ["images.unsplash.com", "via.placeholder.com"],
+    unoptimized: true,
   },
   env: {
     _next_intl_trailing_slash: "1",
