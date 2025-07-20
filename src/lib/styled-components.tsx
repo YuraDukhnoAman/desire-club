0daffb79bc2c7cb235c6e67957499d4910c25a57
YuraDukhnoAman <yura.dukhno@aman.co.il>
2025-07-20 06:47:43 +0300

    implemented

--- src/lib/styled-components.tsx
+++ src/lib/styled-components.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import { useServerInsertedHTML } from "next/navigation";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
-import styled from "styled-components";
 
 export function StyledComponentsRegistry({
   children,
@@ -27,20 +26,3 @@ export function StyledComponentsRegistry({
     </StyleSheetManager>
   );
 }
-
-export const PageContainer = styled.div`
-  min-height: 100vh;
-  padding: ${({ theme }) => theme.spacing.xxl};
-  padding-top: calc(
-    140px + ${({ theme }) => theme.spacing.xxl}
-  ); /* Account for fixed header with large logo */
-  background: ${({ theme }) => theme.colors.background};
-`;
-
-export const PageTitle = styled.h1`
-  color: ${({ theme }) => theme.colors.text};
-  font-family: ${({ theme }) => theme.typography.fontFamily.accent};
-  font-size: ${({ theme }) => theme.typography.fontSize["4xl"]};
-  margin-bottom: ${({ theme }) => theme.spacing.xl};
-  text-align: center;
-`;
